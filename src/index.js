c312428dc9b9842ac0d4554ab9c86f50f4b025ff
stathisR <slagnecro@hotmail.com>
2022-04-07 21:19:43 +0300

    added serviceWorker files

--- src/index.js
+++ src/index.js
@@ -8,6 +8,7 @@ import { searchRobots, requestRobots } from "./reducers";
 import 'tachyons';
 import './index.css';
 import App from "./containers/App";
+import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 
 const logger = createLogger();
 const rootReducer = combineReducers({ searchRobots, requestRobots });
@@ -21,3 +22,8 @@ ReactDOM.render(
   </React.StrictMode>,
   document.getElementById('root')
 );
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://cra.link/PWA
+serviceWorkerRegistration.unregister();
